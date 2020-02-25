fc2889d6958b2a639d1f8b0b53543330466e5a1c
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-25 20:34:13 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -22,21 +22,16 @@ def find_greater_pair(src)
   outer_results=[]
   row_index=0 
   while row_index<src.count do
-   # element_index=0
-   # inner_results=[]
-   # while element_index<src[row_index].count do
       
   if src[row_index][0]>src[row_index][1]
     outer_results<< src[row_index][0]
   else
     outer_results<< src[row_index][1]
   end
-  #element_index+=1 
+  
    row_index+=1 
 end
-#outer_results<< inner_results
-# row_index+=1 
-#end
+
 outer_results
   
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
